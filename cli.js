47a1a51b68d9be1507dd5ea3d18fb8a2cd3061de
hetianyuanjames <hty1016@ad.unc.edu>
2023-04-19 16:21:03 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -30,7 +30,7 @@ const timezone = moment.tz.guess();
 
 // Find the appropriate request URL
 // What variables do I need to construct my URL string? -> Main variables: latitude, longitude, daily, precipitation days & hours
-const days = arg2.d; //"-d" from help text above
+
 
 // Create a latitude & longitude variable (set the following); '||' means or
 var latitude =  arg2.n || arg2.s * -1; // Can these be constants and should I include '()'?
@@ -40,6 +40,8 @@ var longitude = arg2.e || arg2.w * -1;
 // Debugging: Don't forget to concatenate '&daily=precipitation_hours&timezone=' to the URL
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude +'&longitude='+ longitude +'&daily=precipitation_hours&timezone='+ timezone); // What http link do I put here? | replace static values with argv values
 const data = await response.json();
+
+const days = arg2.d; //"-d" from help text above
 
 // Make if-else statement to pass assessment #6 (daily precipitation hours)
 // Intialize an empty string that will be added to (online documentation says to use 'let' to declare a String variable)
